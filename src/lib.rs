f11bb4419b43b766c7e44b44afd5fb5502021b8a
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-09-12 03:01:35 +0200

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -34,9 +34,9 @@ impl<B: Backend> Database<B> {
         let run_id = self.runs.get();
         self.runs.set(run_id + 1);
 
-        let writer = self.backend.write_run(run_id);
+        let w = self.backend.write_run(run_id);
         WriteTransaction {
-            writer,
+            w,
             last_key: Vec::new(),
         }
     }
@@ -58,97 +58,60 @@ impl<'a, B: Backend + 'a> ReadTransaction<'a, B> {
     }
 
     fn read_in_run(&mut self, run_id: u32, key: &[u8], value: &mut [u8]) -> usize {
-        let reader = &mut self.db.backend.read_run(run_id);
+        let r = &mut self.db.backend.read_run(run_id);
 
         let mut key_buf = Vec::new();
 
         // Binary search
         loop {
-            match Self::read_key(reader, &mut key_buf) {
+            match read_key(r, &mut key_buf) {
                 Ok(()) => {}
                 Err(ReadError::Eof) => return 0, // key not present.
             };
 
             // Found?
             let dir = match key_buf[..].cmp(key) {
-                Ordering::Equal => return Self::read_value(reader, value),
+                Ordering::Equal => return read_value(r, value),
                 Ordering::Less => SeekDirection::Right,
                 Ordering::Greater => SeekDirection::Left,
             };
 
             // Not found, do a binary search step.
-            if !reader.binary_search_seek(dir) {
+            if !r.binary_search_seek(dir) {
                 // Can't seek anymore. In this case, the read pointer wasn't moved.
                 // Skip the value from the key we read above, then go do linear search.
-                Self::skip_value(reader);
+                skip_value(r);
                 break;
             }
         }
 
         // Linear search
         loop {
-            match Self::read_key(reader, &mut key_buf) {
+            match read_key(r, &mut key_buf) {
                 Ok(()) => {}
                 Err(ReadError::Eof) => return 0, // key not present.
             };
 
             // Found?
             if key_buf == key {
-                return Self::read_value(reader, value);
+                return read_value(r, value);
             }
 
-            Self::skip_value(reader);
+            skip_value(r);
         }
-    }
-
-    fn read_key(reader: &mut B::RunReader<'_>, buf: &mut Vec<u8, KEY_MAX_SIZE>) -> Result<(), ReadError> {
-        let len = Self::read_leb128(reader)? as usize;
-        assert!(len <= KEY_MAX_SIZE);
-        unsafe { buf.set_len(len) };
-        reader.read(buf).unwrap();
-        Ok(())
-    }
-
-    fn read_value(reader: &mut B::RunReader<'_>, value: &mut [u8]) -> usize {
-        let len = Self::read_leb128(reader).unwrap() as usize;
-        assert!(value.len() >= len);
-        reader.read(&mut value[..len]).unwrap();
-        len
-    }
-
-    fn skip_value(reader: &mut B::RunReader<'_>) {
-        let len = Self::read_leb128(reader).unwrap() as usize;
-        reader.skip(len).unwrap();
-    }
-
-    fn read_u8(reader: &mut B::RunReader<'_>) -> Result<u8, ReadError> {
-        let mut buf = [0u8; 1];
-        reader.read(&mut buf)?;
-        Ok(buf[0])
-    }
-
-    fn read_leb128(reader: &mut B::RunReader<'_>) -> Result<u32, ReadError> {
-        let mut res = 0;
-        let mut shift = 0;
-        loop {
-            let x = Self::read_u8(reader)?;
-            res |= (x as u32 & 0x7F) << shift;
-            if x & 0x80 == 0 {
-                break;
-            }
-            shift += 7;
-        }
-        Ok(res)
     }
 }
 
 pub struct WriteTransaction<'a, B: Backend + 'a> {
-    writer: B::RunWriter<'a>,
+    w: B::RunWriter<'a>,
     last_key: Vec<u8, KEY_MAX_SIZE>,
 }
 
 impl<'a, B: Backend + 'a> WriteTransaction<'a, B> {
     pub fn write(&mut self, key: &[u8], value: &[u8]) {
+        if key.is_empty() {
+            panic!("key cannot be empty.")
+        }
         if key.len() > KEY_MAX_SIZE {
             panic!("key too long.")
         }
@@ -158,32 +121,76 @@ impl<'a, B: Backend + 'a> WriteTransaction<'a, B> {
         }
         self.last_key = Vec::from_slice(key).unwrap();
 
-        let key_len: u32 = key.len().try_into().unwrap();
-        let value_len: u32 = value.len().try_into().unwrap();
-
-        self.write_leb128(key_len);
-        self.writer.write(key);
-        self.write_leb128(value_len);
-        self.writer.write(value);
-        self.writer.record_end();
+        write_record(&mut self.w, key, value)
     }
+}
 
-    fn write_leb128(&mut self, mut val: u32) {
-        loop {
-            let mut part = val & 0x7F;
-            let rest = val >> 7;
-            if rest != 0 {
-                part |= 0x80
-            }
+fn write_record(w: &mut impl RunWriter, key: &[u8], value: &[u8]) {
+    let key_len: u32 = key.len().try_into().unwrap();
+    let value_len: u32 = value.len().try_into().unwrap();
 
-            self.writer.write(&[part as u8]);
+    write_leb128(w, key_len);
+    w.write(key);
+    write_leb128(w, value_len);
+    w.write(value);
+    w.record_end();
+}
 
-            if rest == 0 {
-                return;
-            }
-            val = rest
+fn write_leb128(w: &mut impl RunWriter, mut val: u32) {
+    loop {
+        let mut part = val & 0x7F;
+        let rest = val >> 7;
+        if rest != 0 {
+            part |= 0x80
         }
+
+        w.write(&[part as u8]);
+
+        if rest == 0 {
+            return;
+        }
+        val = rest
     }
+}
+
+fn read_key(r: &mut impl RunReader, buf: &mut Vec<u8, KEY_MAX_SIZE>) -> Result<(), ReadError> {
+    let len = read_leb128(r)? as usize;
+    assert!(len <= KEY_MAX_SIZE);
+    unsafe { buf.set_len(len) };
+    r.read(buf).unwrap();
+    Ok(())
+}
+
+fn read_value(r: &mut impl RunReader, value: &mut [u8]) -> usize {
+    let len = read_leb128(r).unwrap() as usize;
+    assert!(value.len() >= len);
+    r.read(&mut value[..len]).unwrap();
+    len
+}
+
+fn skip_value(r: &mut impl RunReader) {
+    let len = read_leb128(r).unwrap() as usize;
+    r.skip(len).unwrap();
+}
+
+fn read_u8(r: &mut impl RunReader) -> Result<u8, ReadError> {
+    let mut buf = [0u8; 1];
+    r.read(&mut buf)?;
+    Ok(buf[0])
+}
+
+fn read_leb128(r: &mut impl RunReader) -> Result<u32, ReadError> {
+    let mut res = 0;
+    let mut shift = 0;
+    loop {
+        let x = read_u8(r)?;
+        res |= (x as u32 & 0x7F) << shift;
+        if x & 0x80 == 0 {
+            break;
+        }
+        shift += 7;
+    }
+    Ok(res)
 }
 
 #[cfg(test)]
