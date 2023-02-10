72b47c49ac463b562a034370de97c9a803f761a5
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-02-10 17:33:08 +0100

    alloc: use bitfield. 8x less RAM use!

--- src/alloc.rs
+++ src/alloc.rs
@@ -11,7 +11,7 @@ pub enum PageState {
 /// Page allocator
 pub struct Allocator {
     page_count: usize,
-    pages: [PageState; MAX_PAGE_COUNT], // TODO use a bitfield
+    pages: [u8; (MAX_PAGE_COUNT + 7) / 8],
     used: usize,
     next_page_id: usize,
 }
@@ -21,18 +21,37 @@ impl Allocator {
         assert!(page_count <= MAX_PAGE_COUNT);
         Self {
             page_count,
-            pages: [PageState::Free; MAX_PAGE_COUNT],
+            pages: [0u8; (MAX_PAGE_COUNT + 7) / 8],
             used: 0,
             next_page_id: 0, // TODO make random to spread out wear
         }
     }
 
+    pub fn get_bit(&self, i: usize) -> PageState {
+        match (self.pages[i / 8] >> (i % 8)) & 1 {
+            0 => PageState::Free,
+            1 => PageState::Used,
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn set_bit(&mut self, i: usize, state: PageState) {
+        match state {
+            PageState::Free => self.pages[i / 8] &= !(1 << (i % 8)),
+            PageState::Used => self.pages[i / 8] |= 1 << (i % 8),
+        }
+    }
+
     pub fn reset(&mut self) {
         self.used = 0;
-        self.pages.fill(PageState::Free);
+        self.pages.fill(0x00);
     }
 
     pub fn allocate(&mut self) -> PageID {
+        unwrap!(self.try_allocate())
+    }
+
+    pub fn try_allocate(&mut self) -> Option<PageID> {
         let start = self.next_page_id;
         loop {
             let p = self.next_page_id;
@@ -41,53 +60,49 @@ impl Allocator {
                 self.next_page_id = 0;
             }
 
-            let v = &mut self.pages[p];
-            if *v == PageState::Free {
-                *v = PageState::Used;
+            if self.get_bit(p) == PageState::Free {
+                self.set_bit(p, PageState::Used);
                 self.used += 1;
-                return PageID::from_raw(p as RawPageID).unwrap();
+                return Some(PageID::from_raw(p as RawPageID).unwrap());
             }
 
             if self.next_page_id == start {
-                panic!("No free pages"); // TODO
+                return None;
             }
         }
     }
 
     pub fn mark_used(&mut self, page_id: PageID) {
-        assert!(page_id.index() < self.page_count);
+        assert!(page_id.index() < self.page_count, "out of bounds");
+        assert_eq!(self.get_bit(page_id.index()), PageState::Free, "double mark_used");
 
-        assert_eq!(self.pages[page_id.index()], PageState::Free);
-        self.pages[page_id.index()] = PageState::Used;
+        self.set_bit(page_id.index(), PageState::Used);
         self.used += 1;
     }
 
     pub fn free(&mut self, page_id: PageID) {
-        assert!(page_id.index() < self.page_count);
+        assert!(page_id.index() < self.page_count, "out of bounds");
+        assert_eq!(self.get_bit(page_id.index()), PageState::Used, "double free");
 
-        let v = &mut self.pages[page_id.index()];
-        *v = match *v {
-            PageState::Free => panic!("double free"),
-            PageState::Used => PageState::Free,
-        };
+        self.set_bit(page_id.index(), PageState::Free);
         self.used -= 1;
     }
 
     pub fn is_used(&self, page_id: PageID) -> bool {
-        assert!(page_id.index() < self.page_count);
+        assert!(page_id.index() < self.page_count, "out of bounds");
 
-        self.pages[page_id.index()] == PageState::Used
+        self.get_bit(page_id.index()) == PageState::Used
     }
 
     pub fn used_pages(&self) -> usize {
         self.used
     }
 
-    pub(crate) fn free_pages(&self) -> usize {
+    pub fn free_pages(&self) -> usize {
         self.page_count - self.used
     }
 
-    pub(crate) fn page_count(&self) -> usize {
+    pub fn page_count(&self) -> usize {
         self.page_count
     }
 }
